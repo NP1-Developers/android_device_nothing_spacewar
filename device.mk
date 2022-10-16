c71d94a70c8f3c51c77aaf24fbcebd43c184934c
0x5ECF4ULT <github@ox5ecf4ult.pw>
2022-10-16 16:34:06 +0000

    spacewar: add update_engine/verifier for VAB

--- device.mk
+++ device.mk
@@ -14,6 +14,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_verifier
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
